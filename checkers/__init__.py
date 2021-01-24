6abb1b9be195c5b4cf3859880e325538bc5ef51c
goldcvt <thegoldcat5@gmail.com>
2021-01-24 13:17:03 +0300

    added new checker

--- checkers/__init__.py
+++ checkers/__init__.py
@@ -1,4 +1,4 @@
 from checkers.d3d_info_checker import D3DInfoChecker
 from checkers.google_com_checker import GoogleComChecker
 from checkers.ipinfo_io_checker import IPInfoIOChecker
-
+from checkers.httpbin_ip_checker import HTTPBinIPChecker
